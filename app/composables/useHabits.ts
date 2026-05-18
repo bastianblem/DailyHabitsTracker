e0ca6bee9a33cf2787cf18443875a481645c5c90
bastianblem <bastian.buettner@student.uni-luebeck.de>
2026-05-18 07:40:19 +0200

    Added basic logic behind adding, deleting, updating and togglingCompletion of Habits.

--- app/composables/useHabits.ts
+++ app/composables/useHabits.ts
@@ -3,18 +3,28 @@ import { ref } from "vue"
 
 const habits = ref<HabitType[]>([])
 
-export function addHabit() {
+export function addHabit(habit: HabitType) {
+    habits.value.push(habit)
+}
+export function deleteHabit(id: string) {
+    habits.value = habits.value.filter(habit => habit.id !== id)
+}
+export function updateHabit(updatedHabit: HabitType) {
+    habits.value = habits.value.map(h => h.id === updatedHabit.id ? updatedHabit: h)
+    
+}
+export function toggleHabitCompletion(habitId : string) {
+    const today = new Date().toISOString().slice(0, 10)
 
-}
-export function deleteHabit() {
-    
-}
-export function updateHabit() {
-    
-}
-export function toggleHabit() {
-    
-}
-export function toggleHabitCompletion() {
-    
+    habits.value = habits.value.map(h => {
+        if(h.id !== habitId) return h
+        const isCompleted = h.completedDates.includes(today)
+
+        return {
+            ...h,
+            completedDates: isCompleted
+                ? h.completedDates.filter(date => date !== today)
+                : [...h.completedDates, today]
+        }
+    })
 }
