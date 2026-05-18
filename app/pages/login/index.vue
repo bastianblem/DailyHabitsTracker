7f3c6a32cd90dadbaaca28ca936337bfc24f60bf
bastianblem <bastian.buettner@student.uni-luebeck.de>
2026-05-18 08:22:33 +0200

    Added a basic layout for the Guest and Homepage. Setup CTA Buttons for the Login

---
+++ app/pages/login/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <main class="flex flex-col justify-center items-center gap-2 mt-28">
+        <h1 class="font-bold text-4xl">Willkommen auf der Seite für Gäste!</h1>
+        <p>Mach es dir ruhig bequem, aber hier gibt es nichts zu sehen oder zu machen.</p>
+        <div class="flex justify-center items-center gap-2 mt-4">
+            <button class="bg-black/80 text-white font-medium px-4 py-2 rounded-full border border-black transition hover:bg-black hover:scale-105">Zum Login</button>
+            <button class="bg-amber-500/80 font-medium px-4 py-2 rounded-full border border-black transition hover:bg-amber-500 hover:scale-105">Registrieren</button>
+        </div>
+    </main>
+</template>
+<script setup lang="ts"></script>
